621f4f74b17f818304540c9826aec609a9162ae0
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2025-07-02 17:00:35 +0530

    add cors of domain

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: ['https://only-frontend-r34w.vercel.app', 'http://localhost:5173'],
+  origin: ['https://www.svrtc.com','https://only-frontend-r34w.vercel.app', 'http://localhost:5173'],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
